e8cfa94a7acf92500fe6ccbb046a5e1a2b07b47e
christian-nieves <thechristiannieves@gmail.com>
2026-05-26 11:56:05 -0400

    Premium Topping: Constructor, getters & setters done. (Need to do getprice method)

---
+++ src/main/java/com/pluralsight/PremiumTopping.java
@@ -0,0 +1,31 @@
+package com.pluralsight;
+
+public class PremiumTopping extends Topping {
+    private String cheeseOrMeat;
+    private boolean extraTopping;
+
+    // Constructor
+    public PremiumTopping(String name, String cheeseOrMeat, boolean extraTopping) {
+        super(name);
+        this.cheeseOrMeat = cheeseOrMeat;
+        this.extraTopping = extraTopping;
+    }
+
+    // Getters and Setters
+    public String getCheeseOrMeat() {
+        return cheeseOrMeat;
+    }
+
+    public void setCheeseOrMeat(String cheeseOrMeat) {
+        this.cheeseOrMeat = cheeseOrMeat;
+    }
+
+    public boolean isExtraTopping() {
+        return extraTopping;
+    }
+
+    public void setExtraTopping(boolean extraTopping) {
+        this.extraTopping = extraTopping;
+    }
+
+}
