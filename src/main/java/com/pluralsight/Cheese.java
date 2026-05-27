06a757a2cfd6a3a349a8e7f34c12b9391de065fd
christian-nieves <thechristiannieves@gmail.com>
2026-05-27 10:22:29 -0400

    Cheese class done

---
+++ src/main/java/com/pluralsight/Cheese.java
@@ -0,0 +1,31 @@
+package com.pluralsight;
+
+public class Cheese extends PremiumTopping {
+    public Cheese(String name, String cheeseOrMeat, boolean extraTopping) {
+        super(name, cheeseOrMeat, extraTopping);
+    }
+
+    // Override Method
+    @Override
+    public double getPrice(int toppingAmount) {
+        double basePrice = 0;
+        double extraPrice = 0;
+
+        if (toppingAmount == 8) {
+            basePrice = 0.75;
+            extraPrice = 0.30;
+        } else if (toppingAmount == 12) {
+            basePrice = 1.50;
+            extraPrice = 0.60;
+        } else if (toppingAmount == 16) {
+            basePrice = 2.25;
+            extraPrice = 0.90;
+        }
+
+        if (isExtraTopping()) {
+            return basePrice + extraPrice;
+        } else {
+            return basePrice;
+        }
+    }
+}
