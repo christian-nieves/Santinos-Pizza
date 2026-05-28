c8559924ce9bcce3030028a88ece288168cb61a9
christian-nieves <thechristiannieves@gmail.com>
2026-05-28 09:46:08 -0400

    Drink Class: Finished (completed methods)

--- src/main/java/com/pluralsight/Drink.java
+++ src/main/java/com/pluralsight/Drink.java
@@ -27,4 +27,30 @@ public class Drink {
     public void setFlavor(String flavor) {
         this.flavor = flavor;
     }
+
+    // Methods
+
+    public double getPrice() {
+
+        if (size.equalsIgnoreCase("small")) {
+            return 2.00;
+        }
+
+        else if (size.equalsIgnoreCase("medium")) {
+            return 2.50;
+        }
+
+        else if (size.equalsIgnoreCase("large")) {
+            return 3.00;
+        }
+
+        else { System.err.println("Invalid drink size, please try again.");
+            return 0; // prints red message alerting user input was invalid
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Drink: " + flavor + "\nSize: " + size;
+    }
 }
