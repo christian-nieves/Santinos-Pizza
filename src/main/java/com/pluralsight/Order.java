ecd90f371fa0a1c970fd08a557e09efe401b2333
christian-nieves <thechristiannieves@gmail.com>
2026-05-28 11:46:22 -0400

    Order Class: Complete (Methods Done)

--- src/main/java/com/pluralsight/Order.java
+++ src/main/java/com/pluralsight/Order.java
@@ -35,4 +35,51 @@ public class Order {
         return orderDate;
     }
 
+    // Methods
+    public void addPizza(Pizza pizza) {
+        pizzas.add(pizza);
+    }
+
+    public void addDrink(Drink drink) {
+        drinks.add(drink);
+    }
+
+    public void addGarlicKnots(GarlicKnots garlicKnots1) {
+        garlicKnots.add(garlicKnots1);
+    }
+
+    public double getTotal() {
+        double total = 0;
+
+        for (Pizza pizza : pizzas) {
+            total += pizza.getPrice();
+        }
+        for (Drink drink : drinks) {
+            total += drink.getPrice();
+        }
+        for (GarlicKnots garlicKnot : garlicKnots) {
+            total += garlicKnot.getPrice();
+        }
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        String orderResult =  "Order Date: " + orderDate;
+
+        if (!pizzas.isEmpty()) {
+            orderResult += "\nPizzas: " + pizzas;
+        }
+
+        if (!drinks.isEmpty()) {
+            orderResult += "\nDrinks: " + drinks;
+        }
+
+        if (!garlicKnots.isEmpty()) {
+            orderResult += "\nGarlic Knots: " + garlicKnots;
+        }
+
+        orderResult += "\nTotal: $" + getTotal();
+        return orderResult;
+    }
 }
