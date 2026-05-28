de1c63444c90407c8b7da986e1d7350f24ca9568
christian-nieves <thechristiannieves@gmail.com>
2026-05-28 19:15:08 -0400

    UserInterface: display method done

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -10,5 +10,28 @@ public class UserInterface {
         scanner = new Scanner(System.in);
     }
 
+    public void display() {
+        boolean quit = false;
+        while (!quit) {
+            System.out.println("========== WELCOME TO SANTINO'S PIZZA ==========");
+            System.out.println("1) New Order");
+            System.out.println("0) Exit");
+
+            System.out.println("Enter your choice: ");
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+
+                    break;
+                case "0":
+                    quit = true;
+                    break;
+                default:
+                    System.err.println("Invalid option. Please try again.");
+            }
+        }
+    }
+
 
 }
